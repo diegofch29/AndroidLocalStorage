ba11341e5360f9b795253190b0fe51c5be77298b
Diego Fernando Chinchilla Bautista <dchinchilla@flar.net.co>
2021-05-06 12:49:04 -0500

    commit

--- app/src/main/java/co/edu/eci/ieti/android/network/RetrofitNetwork.java
+++ app/src/main/java/co/edu/eci/ieti/android/network/RetrofitNetwork.java
@@ -8,22 +8,41 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * @author Santiago Carrillo
  * 4/23/19.
  */
-public class RetrofitNetwork
-{
+public class RetrofitNetwork{
 
     private AuthService authService;
 
+    private TaskService taskService;
 
-    public RetrofitNetwork()
-    {
+
+    public RetrofitNetwork(){
         Retrofit retrofit = new Retrofit.Builder().baseUrl( "http:/10.0.2.2:8080/" ) //localhost for emulator
             .addConverterFactory( GsonConverterFactory.create() ).build();
 
         authService = retrofit.create( AuthService.class );
     }
 
-    public AuthService getAuthService()
-    {
+    public RetrofitNetwork( final String token ){
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor( new Interceptor(){
+
+            @Override
+            public okhttp3.Response intercept( Chain chain ) throws IOException{
+                Request original = chain.request();
+
+                Request request = original.newBuilder().header( "Accept", "application/json" ).header( "Authorization",
+                                                                                                       "Bearer "
+                                                                                                           + token ).method(
+                    original.method(), original.body() ).build();
+                return chain.proceed( request );
+            }
+        } );
+        Retrofit retrofit =
+            new Retrofit.Builder().baseUrl( BASE_URL ).addConverterFactory( GsonConverterFactory.create() ).client(
+                httpClient.build() ).build();
+    }
+
+    public AuthService getAuthService(){
         return authService;
     }
 }
